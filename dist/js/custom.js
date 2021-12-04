2ed1c24035add0bc66c9b3a973672556f411b746
mohamedmansy316 <mohamedmansy855@gmail.com>
2021-12-04 18:20:17 +0200

    add

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,13 +1,21 @@
-let book = document.querySelectorAll(".item");
 
-function bodySize(){
-       let windowWidth = window.innerWidth;
-book.forEach((e)=>{
-       if(windowWidth >= 600){
-              e.setAttribute("data-merge", 3);
-       }else{
-              e.setAttribute("data-merge", 6);
-       }
-})
+
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
-window.addEventListener("resize", bodySize);
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
